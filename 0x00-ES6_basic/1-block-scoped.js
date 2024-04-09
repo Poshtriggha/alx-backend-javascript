2b54c5c8ffc15ed1647efa6d5398d25ab4768e98
“Poshtriggha” <mrposhboo@gmail.com>
2024-04-09 02:29:02 +0000

    new

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    return [true, false];
+    // Remove the redeclarations of task and task2
+    // They will retain their initial values defined outside the conditional block
   }
 
   return [task, task2];
