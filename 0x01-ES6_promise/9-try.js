d47d4aa50059a08e7cec392a670cb9922c16ccc6
posh <mrposhboo@gmail.com>
2024-04-11 00:43:39 +0100

    processed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,16 @@
-export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      const result = mathFunction();
-      queue.push(result);
-    } catch (error) {
-      queue.push(error.message);
-      throw error;
-    }
+function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
     queue.push('Guardrail was processed');
-    return queue;
   }
+
+  return queue;
+}
+
+export default guardrail;
