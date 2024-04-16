17dee66838be186b97c3fdc20515d4638ed8dd53
“Poshtriggha” <mrposhboo@gmail.com>
2024-04-16 18:26:43 +0000

    npm

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,9 @@
 function getListStudents() {
-    var arrayOfObjects = [];
-
-    arrayOfObjects.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-    arrayOfObjects.push({ id: 2, firstName: 'James', location: 'Columbia' },);
-    arrayOfObjects.push({ name: 'Bob', age: 35 });
-
-    return arrayOfObjects;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
 
-var result = getListStudents();
-
-console.log(result);
+export default getListStudents;
