e70049b1d1679b0fc762a28f14f018b7b0892d04
posh <mrposhboo@gmail.com>
2024-06-07 22:17:24 +0100

    ddd

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,21 +1,21 @@
 function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
-
-    if (type === 'SUM') {
-        return rounded_a + rounded_b;
-    }
-
-    if (type === 'SUBTRACT') {
-        return rounded_a - rounded_b;
-    }
-
-    if (type === 'DIVIDE') {
-        if (rounded_b === 0) {
-            return 'Error'
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+  
+    switch (type) {
+      case 'SUM':
+        return roundedA + roundedB;
+      case 'SUBTRACT':
+        return roundedA - roundedB;
+      case 'DIVIDE':
+        if (roundedB === 0) {
+          return 'Error';
         }
-        return rounded_a / rounded_b;
+        return roundedA / roundedB;
+      default:
+        throw new Error('Invalid type');
     }
-}
-
-module.exports = calculateNumber;
+  }
+  
+  module.exports = calculateNumber;
+  
